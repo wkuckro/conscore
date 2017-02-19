3c31c66544a7612d08ea6afd781431555b7ec99d
Will Kuckro <wkuckro@luc.id>
2017-02-18 22:49:17 -0600

    printing results

--- get.py
+++ get.py
@@ -1,8 +1,9 @@
 import requests
-
+import json
 
 headers = {'X-API-Key': 'd939NE7Ksk6mSmDxT1r5za8BBw4GaBBz8ndMmdaU'}
 response = requests.get("https://api.propublica.org/congress/v1/115/senate/members.json", headers=headers)
 response.json()
 # Print the status code of the response.
+print(response.status_code)
 print(response.json())
